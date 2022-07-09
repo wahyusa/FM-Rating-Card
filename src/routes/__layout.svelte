e4a82a2c7e9690037a84a3c09a78ddfde673676c
Wahyu Syamsul A'lam <wahyuskull22@gmail.com>
2022-07-09 04:39:47 +0000

    finally works on ghpages

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,6 +3,11 @@
 </script>
 
 <svelte:head>
-	<link rel="icon" type="image/png" sizes="32x32" href="./images/favicon-32x32.png" />
+	<title>Frontend Mentor | Interactive rating component</title>
+	<link rel="icon" type="image/png" sizes="32x32" href="FM-Rating-Card/images/favicon-32x32.png" />
 </svelte:head>
 <slot />
+<div class="absolute bottom-0 text-brand-medium-grey text-sm">
+    Challenge by <a class="hover:underline" href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+    Coded by <a class="hover:underline" href="https://github.com/wahyusa/FM-Rating-Card">Wahyu Syamsul A'lam</a>.
+</div>
