aa6223e7117b46a7cb81fe8642afa16e0e5eda78
Wahyu Syamsul A'lam <wahyuskull22@gmail.com>
2022-07-11 04:44:24 +0000

    set alt tag value blank for not necessary img

--- src/components/ThankYou.svelte
+++ src/components/ThankYou.svelte
@@ -8,7 +8,7 @@
 	<img
 		class="my-2"
 		src="/FM-Rating-Card/images/illustration-thank-you.svg"
-		alt="illustration-thank-you"
+		alt=""
 	/>
 	<span class="rounded-full text-brand bg-brand-medium-grey/20 px-3 py-1.5">
 		You selected {ratingValue} out of 5
